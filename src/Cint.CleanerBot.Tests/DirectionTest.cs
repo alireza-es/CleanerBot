7115febdd3b8823868cba9f785fd77b78535091c
AliReza <alireza.es@gmail.com>
2019-12-21 16:46:42 +0330

    Add Console app User Interface to the project

---
+++ src/Cint.CleanerBot.Tests/DirectionTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Cint.CleanerBot.Tests
+{
+    public class DirectionTest
+    {
+        [Theory]
+        [InlineData("E", Direction.East)]
+        [InlineData("W", Direction.West)]
+        [InlineData("N", Direction.North)]
+        [InlineData("S", Direction.South)]
+        public void ParseDirection_ValidInput_ParseSuccess(string input, Direction expected)
+        {
+            //arrange
+
+            //act
+            var actual = input.ParseDirection();
+
+            //check
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- src/Cint.CleanerBot.UI/Program.cs
+++ src/Cint.CleanerBot.UI/Program.cs
@@ -4,9 +4,32 @@ namespace Cint.CleanerBot.UI
 {
     class Program
     {
+        private const string Separator = " ";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of Commands:");
+            var numberOfCommands = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the start point (x y) of robot with a white space separator:");
+            var startPointArray = Console.ReadLine().Split(Separator);
+
+            var startX = int.Parse(startPointArray[0]); 
+            var startY = int.Parse(startPointArray[1]);
+
+            var robot = new Robot(startX, startY);
+
+            for (var i = 0; i < numberOfCommands; i++)
+            {
+                Console.Write($"Enter the direction (E,W,N,S) of command #{i+1} then steps count with a white space separator:");
+                var commandArray = Console.ReadLine().Split(Separator);
+                var direction = commandArray[0].ParseDirection();
+                var step = int.Parse(commandArray[1]);
+
+                robot.Move(direction, step);
+            }
+
+            Console.WriteLine("\n***** Finished *****\n");
+            Console.WriteLine($"=> Cleaned: {robot.GetCleanedLocations()}");
         }
     }
 }
