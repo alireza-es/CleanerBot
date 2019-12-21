dc1863642cfcea955c9c903c273b1e9ae18a6322
AliReza <alireza.es@gmail.com>
2019-12-21 11:59:23 +0330

    Start From a Red Test Create RobotTest

---
+++ src/Cint.CleanerBot.Tests/RobotTest.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Cint.CleanerBot.Tests
+{
+    public class RobotTest
+    {
+        [Fact]
+        public void Robot_CreateObject_ObjectCreatedInStartingLocation()
+        {
+            //arrange
+            const int startingX = 10;
+            const int startingY = 22;
+
+            //act
+            var robot = new Robot(startingX, startingY);
+
+            //check
+            Assert.Equal(startingX, robot.CurrentLocation.X);
+            Assert.Equal(startingY, robot.CurrentLocation.Y);
+        }
+    }
+
+    public class Robot
+    {
+        public Robot(int x, int y)
+        {
+
+        }
+
+        public Point CurrentLocation { get; set; }
+    }
+
+    public class Point
+    {
+        public int Y { get; set; }
+        public int X { get; set; }
+    }
+}
