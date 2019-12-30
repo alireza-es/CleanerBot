dc49d77fba203ba1039c170d3b8993514b058216
AliReza <alireza.es@gmail.com>
2019-12-30 20:08:52 +0330

    Add Integration Test to the project Start the "dotnet" process and run the console app using it Send inputs and check outputs using StandardInput and RedirectStandardOutput

---
+++ src/Cint.CleanerBot.IntegrationTests/CleanerBotTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+namespace Cint.CleanerBot.IntegrationTests
+{
+    public class CleanerBotTest
+    {
+        [Fact]
+        public void CleanerBot_MainScenario_CleanedSuccessfully()
+        {
+            //arrange
+#if DEBUG
+            const string appDllPath = @"..\..\..\..\Cint.CleanerBot.UI\bin\Debug\netcoreapp3.0\Cint.CleanerBot.UI.dll";
+#else
+            const string appDllPath = @"..\..\..\..\Cint.CleanerBot.UI\bin\Release\netcoreapp3.0\Cint.CleanerBot.UI.dll";
+#endif
+            var processInfo = new ProcessStartInfo("dotnet", appDllPath);
+            processInfo.RedirectStandardInput = true;
+            processInfo.RedirectStandardOutput = true;
+
+
+            //act
+            var process = Process.Start(processInfo);
+
+            process.StandardInput.WriteLine("2");
+            process.StandardInput.WriteLine("10 22");
+            process.StandardInput.WriteLine("E 2");
+            process.StandardInput.WriteLine("N 1");
+
+            //check
+            Assert.Equal("=> Cleaned: 4", process.StandardOutput.ReadLine());
+        }
+    }
+}
